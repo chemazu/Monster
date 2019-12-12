5c8be3f406547b9f05a2b057a783efcdcbc6a83c
chemazu <chukwuemekachhemazu@gmail.com>
2019-12-12 14:25:37 +0100

    Updates

---
+++ precache-manifest.145bd1c5aed59f20c0b4e26d95878eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77160b8e10b027692e6fd9b188a661d4",
+    "url": "https//chemazu.github.io/Monster/index.html"
+  },
+  {
+    "revision": "08c0041109788579fecf",
+    "url": "https//chemazu.github.io/Monster/static/css/main.d7858bdb.chunk.css"
+  },
+  {
+    "revision": "c0e2c677b0af455d23cd",
+    "url": "https//chemazu.github.io/Monster/static/js/2.ff8926b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https//chemazu.github.io/Monster/static/js/2.ff8926b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08c0041109788579fecf",
+    "url": "https//chemazu.github.io/Monster/static/js/main.c0aa5a7c.chunk.js"
+  },
+  {
+    "revision": "eddb946a434c01518fb7",
+    "url": "https//chemazu.github.io/Monster/static/js/runtime-main.f4ec44db.js"
+  }
+]);
